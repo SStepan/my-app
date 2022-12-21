feaec2f312f1478281aa4cbc4cd6ba64bbd3bbc2
SStepan <myronyuk.s@gmail.com>
2022-12-22 01:13:27 +0200

    start header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
 import './Header.scss'
+import HamburgerImg from '../images/hamburger-menu.svg'
+import YoutubeImg from '../images/youtube-logo.svg'
+import SearchImg from '../images/search.svg'
 
 export default function Header() {
     return (
-        <div className="left-section">Hello</div>
+        <div className='header'>
+            <div className="left-section">
+                <img className='hamburger-img' src={HamburgerImg} />
+                <img className='youtube-img' src={YoutubeImg} />
+            </div>
+
+            <div className='middle-section'>
+                <input placeholder='Search' className='search-input' type="text" />
+                <button className='search-button'>
+                    <img className='search-img' src={SearchImg} alt="" />
+                </button>
+
+            </div>
+
+            <div className='right-section'>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </div>
     )
 }
