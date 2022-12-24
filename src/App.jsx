754821f32b9964c7d62e8fe972480f9164def088
SStepan <myronyuk.s@gmail.com>
2022-12-24 20:49:48 +0200

    add main images

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Test from './components/Test'
 import Sidebar from './components/Sidebar';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Sidebar />
+      <Main />
       {/* <Test /> */}
     </div>
   );
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import './Main.scss'
+
+export default function Main() {
+    return (
+        <div className='main'>
+            Hello world
+        </div>
+    )
+}
